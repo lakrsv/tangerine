26206be5b149409207c01959fdc8e166ca84cf15
Lars-Kristian Svenoy <lakrsv@gmail.com>
2023-03-23 12:56:59 +0000

    Create encrypted files...

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,4 +1,10 @@
-use std::env;
+use aes_gcm::{
+    aead::{Aead, KeyInit},
+    Aes256Gcm, Nonce
+};
+use std::fs::File;
+use std::io::{BufRead, Write};
+use std::{env, io};
 
 include!(concat!(env!("OUT_DIR"), "/id.rs"));
 
@@ -7,7 +13,63 @@ pub fn run() {
     if args[1] == "Show me the magic Tangerine" {
         println!("{}", client_id());
         return;
+    } else if args[1] == "Create me a Tangerine" {
+        create_tangerine();
+        return;
+    } else if args[1] == "Hide my Tangerine" {
+        hide_tangerine();
+        return;
     }
+
     println!("Client id is {}", client_id());
     println!("Encryption key is {}", encryption_key());
+}
+
+fn create_tangerine() {
+    let mut file = File::create(format!("./commands/{}", client_id())).unwrap();
+    file.write(b"TANGERINE\n// ADD TANGERINES\n\n!TANGERINE")
+        .unwrap();
+}
+
+fn hide_tangerine() {
+    let file =
+        File::open(format!("./commands/{}", client_id())).expect("Expected command file to exist");
+    let mut lines = io::BufReader::new(file).lines();
+
+    // Skip header
+    let header = lines.next().unwrap().unwrap();
+    if header != "TANGERINE" {
+        eprintln!("Expected tangerine header");
+    }
+
+    let key = encryption_key();
+    let nonce = Nonce::from_slice(b"TANGERINE!!!");
+
+    println!("{}", key);
+    let cipher = Aes256Gcm::new_from_slice(key.as_bytes()).unwrap();
+
+    let mut encrypted_lines = vec![];
+
+    for line in lines {
+        let line = line.unwrap();
+        if line.starts_with("//") || line.starts_with("!") {
+            // Skip comment
+            continue;
+        }
+
+        let ciphertext = cipher.encrypt(nonce, line.as_bytes()).unwrap();
+        encrypted_lines.push(ciphertext);
+    }
+
+    let mut file = File::create(format!("./commands/{}.tangerine", client_id())).unwrap();
+    file.write(b"TANGERINE_ENC\n");
+    for line in encrypted_lines {
+        file.write(&line);
+        file.write(b"\n");
+    }
+    file.write(b"!TANGERINE_ENC");
+}
+
+fn read_tangerine(){
+    
 }
